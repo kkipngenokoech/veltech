63e094440f997f876365f4ffae5b51c58c787afc
kip <kkipngenokoech22@gmail.com>
2024-02-02 14:46:09 +0300

    material ui is working

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,21 @@
 // pages/index.js
 
+import { Button } from '@mui/material';
 import React from 'react';
 
-const Home = () => (
+function Home() {
+  return (
   <div>
-    <h1 className='text-blue font-bold'>Welcome to the Home Page</h1>
-    {/* Add more HTML/JSX here for your landing page */}
+      <h1 className='text-blue-500 font-bold'>Welcome to the Home Page</h1>
+      <Button variant="contained" color="primary">
+  Hello World
+</Button>
+
   </div>
-);
+  )
+}
+
+
+
 
 export default Home;
