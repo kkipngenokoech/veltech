a80e7da51a56c440d172f2d59083476e551d6147
kip <kkipngenokoech22@gmail.com>
2024-02-03 10:06:55 +0300

    about section in landing page added

--- src/app/pages/landingpage.js
+++ src/app/pages/landingpage.js
@@ -27,7 +27,7 @@ export default function LandingPage() {
     setIndex((index + 1) % heros.length);
   }
   return (
-    <div>
+    <div className="bg-beige">
       <div className="bg-hero-pattern bg-cover h-screen flex items-center justify-center relative">
         <span className="absolute left-5 transform text-lg font-bold text-blue-500 w-full md:w-1/3 animate-fade-in-out leading-loose">
           {heros[index]}
@@ -41,9 +41,12 @@ export default function LandingPage() {
           Hello World
         </Button>
       </div>
-          <div>
+          <div className="bg-beige p-8 py-24 sm:py-32 ">
               <StatsSection />
-      </div>
+          </div>
+          <div>
+              <AboutUsSection />
+          </div>
     </div>
   );
 }
@@ -57,7 +60,7 @@ const stats = [
 
 function StatsSection() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white py-24 sm:py-32 rounded-lg shadow-lg">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
           {stats.map((stat) => (
@@ -79,4 +82,117 @@ function StatsSection() {
       </div>
     </div>
   );
+}
+
+
+
+//! about us section
+
+const posts = [
+  {
+    id: 1,
+    title: 'Our Story',
+    href: '#',
+    description:
+      "EverydayVisa was born from a shared passion for capturing life's everyday beauty. Our journey started with a simple idea: to create a platform where individuals could seamlessly share and celebrate their daily adventures through captivating visual stories. Today, EverydayVisa is a vibrant community, weaving together diverse experiences from around the globe, turning ordinary moments into extraordinary memories.",
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Our Story', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    },
+    {
+    id: 2,
+    title: 'Vision for the Community',
+    href: '#',
+    description:
+      "At EverydayVisa, our vision is to foster a global community connected by a shared love for exploration and storytelling. We envision a platform where individuals from all walks of life come together to celebrate the richness of daily experiences. Our goal is to empower and inspire our community to discover, share, and connect through the universal language of visual storytelling, making EverydayVisa a cherished space for creating, preserving, and reliving meaningful memories.",
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Community', href: '#' },
+    author: {
+      name: 'Luka Ayling',
+      role: 'Community Manager',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    },
+    {
+    id: 3,
+    title: 'Founder\'s Message',
+    href: '#',
+    description:
+      "Hello EverydayVisa Community,Welcome to a space where ordinary moments become extraordinary stories. As the founder, I invite you to share, connect, and celebrate the beauty of your day-to-day adventures. Together, let's turn every snapshot into a cherished memory.\nHappy exploring!",
+    date: 'Mar 16, 2020',
+    datetime: '2020-03-16',
+    category: { title: 'Marketing', href: '#' },
+    author: {
+      name: 'Michael Foster',
+      role: 'Co-Founder / CTO',
+      href: '#',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    },
+    
+  // More posts...
+]
+
+function AboutUsSection() {
+  return (
+    <div className="bg-beige py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">About EverydayVisa</h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Explore the world through EverydayVisa, where we transform daily moments into shared visual stories of adventure and connection.
+          </p>
+        </div>
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {posts.map((post) => (
+            <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+              <div className="flex items-center gap-x-4 text-xs">
+                <time dateTime={post.datetime} className="text-gray-500">
+                  {post.date}
+                </time>
+                <a
+                  href={post.category.href}
+                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                >
+                  {post.category.title}
+                </a>
+              </div>
+              <div className="group relative">
+                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  <a href={post.href}>
+                    <span className="absolute inset-0" />
+                    {post.title}
+                  </a>
+                </h3>
+                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
+              </div>
+              <div className="relative mt-8 flex items-center gap-x-4">
+                <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
+                <div className="text-sm leading-6">
+                  <p className="font-semibold text-gray-900">
+                    <a href={post.author.href}>
+                      <span className="absolute inset-0" />
+                      {post.author.name}
+                    </a>
+                  </p>
+                  <p className="text-gray-600">{post.author.role}</p>
+                </div>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,17 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: (theme) => ({
-        // Image by <a href="https://www.freepik.com/free-photo/friends-enjoying-their-life-offline_23369393.htm#query=friends%20photos%20i%20can%20use%20for%20hero%20section%20for%20my%20website&position=13&from_view=search&track=ais&uuid=39fbe5f8-7280-48bc-8c37-a56db5279e42">Freepik</a>
         "hero-pattern": "url('/images/hero-section.jpg')",
-        // "footer-texture": "url('/img/footer-texture.png')",
       }),
+      colors: {
+        beige: '#efeae7', // Replace with the exact shade of beige you want to use
+        beigeBG: "#c39d99"
+      },
     },
   },
   plugins: [],
