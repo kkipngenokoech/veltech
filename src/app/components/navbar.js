d9307cc40aeb64d8ab1845d81efd3c34ee1987d3
kip <kkipngenokoech22@gmail.com>
2024-02-07 16:02:34 +0300

    auth for mobile version complete

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -117,18 +117,18 @@ export default function VelTechNavbar() {
               </div>
               {user ? (
                 <div>
-                  <a
-                    href="#"
+                  <button
+                   onClick={handleSignout}
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Sign Out
-                  </a>
+                  </button>
               </div>
               ): (
                 
                 <div className="py-6">
                   <a
-                    href="#"
+                    href="/auth?authType=signin"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Get Started
